84c72b4c9abfb82f9df2345608e6bc876e8cc632
Madan <madanmohan7272906844@gmail.com>
2024-05-12 00:25:12 +0530

    .auth-redirect

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -61,44 +61,6 @@ export const Navbar = () => {
     //   </div>
     // </div>
 
-
-
-
-
-
-    // <nav class="bg-gray-900 border-gray-200 dark:bg-gray-900">
-    //   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    //     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse" onClick={handleBlogAppClick}>
-    //       <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-    //       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BlogApp</span>
-    //     </a>
-    //     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-    //       <span class="sr-only">Open main menu</span>
-    //       <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-    //         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-    //       </svg>
-    //     </button>
-    //     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-    //       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-    //         <li>
-    //           <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-    //         </li>
-    //         <li>
-    //           <a href="/my-blogs" class="block py-2 px-3 text-white-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white hover:bg-blue-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">My Blogs</a>
-    //         </li>
-    //         <li>
-    //           <a href="/create-post" class="block py-2 px-3 text-white-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white hover:bg-blue-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create new Post</a>
-    //         </li>
-    //         <li>
-    //           <a href="/saved-blogs" class="block py-2 px-3 text-white-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white hover:bg-blue-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Saved Blogs</a>
-    //         </li>
-    //         <li>
-    //           <a href="" className='logout-btn' onClick={logout} class="block py-2 px-3 text-white-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 hover:bg-blue-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><span class="text">Logout ({window.localStorage.getItem('Username')})</span> </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
     <nav class="bg-gray-900 border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse" onClick={handleBlogAppClick}>
@@ -118,7 +80,7 @@ export const Navbar = () => {
                 <a href="/" class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700" aria-current="page">Home</a>
               </li>
               <li>
-                <a href="" onClick={login} class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700 " aria-current="page">Login/Register</a>
+                <button onClick={login} class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700 " aria-current="page">Login/Register</button>
               </li>
             </ul>
           ) :
