09c6934b92c54741d8315ba3345ba0bdf241c6f5
Madan <madanmohan7272906844@gmail.com>
2024-05-11 23:57:35 +0530

    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -115,7 +115,7 @@ export const Navbar = () => {
           {!cookies.access_Token ? (
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a href="/" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500 hover:bg-blue-700" aria-current="page">Home</a>
+                <a href="/" class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700" aria-current="page">Home</a>
               </li>
               <li>
                 <a href="" onClick={login} class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700 " aria-current="page">Login/Register</a>
@@ -124,7 +124,7 @@ export const Navbar = () => {
           ) :
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a href="/" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500 hover:bg-blue-700" aria-current="page">Home</a>
+                <a href="/" class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700" aria-current="page">Home</a>
               </li>
               <li>
                 <a href="/my-blogs" class="block py-2 px-3 text-white-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 hover:bg-blue-700 " aria-current="page">My Blogs</a>
